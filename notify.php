1036c817100302469c98f895781f6afaec005b60
george-labbmiljo <george.labbmiljo@gmail.com>
2020-08-19 03:48:37 -0700

    Add test failure validation

--- notify.php
+++ notify.php
@@ -6,7 +6,7 @@ $fileName    = "data/Test_". $currentDate .".csv";
 if(file_exists($fileName))
 {
     $date       = $currentDate;
-    $server     = "RDP-1";
+    $server     = "DEVELOPMENT";
     $status     = "OK";
     $type       = "Regular";
     $requestUrl = "https://hook.integromat.com/54u8xjckpokitid88ss5xm6qczu71506";
@@ -20,10 +20,33 @@ if(file_exists($fileName))
         'type'  => $type
     );
 
-    $str = http_build_query($formData);
+    sendData($formData, $requestUrl);
+
+
+}else{
+    $date       = $currentDate;
+    $server     = "DEVELOPMENT";
+    $status     = "NOT OK";
+    $type       = "Regular";
+    $requestUrl = "https://hook.integromat.com/54u8xjckpokitid88ss5xm6qczu71506";
+    $url        = "https://george-labbmiljo.github.io/pms/88f8adf2530eba177361d604a43acf6d32bb2d10/".$currentDate."/index.html";
+
+    $formData = array(
+        'date'   => $date,
+        'url'    => $url, 
+        'status' => $status,
+        'server' => $server,
+        'type'  => $type
+    );
+    sendData($formData, $requestUrl);
+}
+
+function sendData($data, $rURL)
+{
+    $str = http_build_query($data);
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $requestUrl);
+    curl_setopt($ch, CURLOPT_URL, $rURL);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $str);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
